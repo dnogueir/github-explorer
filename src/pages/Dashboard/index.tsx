86f3ab9c09925f4811763664fd105a14e04793bd
dnogueir <daniell_dng@hotmail.com>
2020-05-11 19:25:35 -0300

    treating errors

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,62 +1,78 @@
-import React from 'react';
-import {Title, Form, Repositories} from './styles';
+import React, {useState, FormEvent} from 'react';
+import {Title, Form, Repositories, Error} from './styles';
 import logoImg from '../../assets/logo.svg';
 import {FiChevronRight} from 'react-icons/fi';
+import api from '../../services/api';
+
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    }
+}
 
 const Dashboard: React.FC = () => {
+    const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+
+        event.preventDefault();
+
+        if(!inputError) {
+            setInputError('Digite o nome do autor/repositório');
+            return;
+        }
+
+        try {
+            const response = await api.get<Repository>(`repos/${newRepo}`);
+
+            const repository = response.data;
+
+            setRepositories([...repositories, repository]);
+            setNewRepo('');
+            setInputError('');
+        } catch (err) {
+            setInputError('Erro na busca por esse repositório');
+        }
+    }
+
     return (
         <>
             <img src={logoImg} alt="Github Explorer"/>
             <Title>Explore repositórios no github</Title>
 
-            <Form>
-                <input placeholder="Digite o nome do repositório"/>
+            <Form hasError={Boolean(inputError)} onSubmit={handleAddRepository}>
+                <input
+                    value={newRepo}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome do repositório"
+                />
                 <button type="submit">pesquisar</button>
             </Form>
 
-            <Repositories>
-                <a href="teste">
-                    <img
-                        src={logoImg}
-                        alt="Daniel Nogueira"
-                    />
-                    <div>
-                        <strong>dnogueir/node-challenge</strong>
-                        <p>Node challenge description</p>
-                    </div>
-
-                    <FiChevronRight size={20}/>
-                </a>
-            </Repositories>
+            { inputError && <Error>{inputError}</Error> }
 
             <Repositories>
-                <a href="teste">
-                    <img
-                        src={logoImg}
-                        alt="Daniel Nogueira"
-                    />
-                    <div>
-                        <strong>dnogueir/node-challenge</strong>
-                        <p>Node challenge description</p>
-                    </div>
+                {
+                    repositories.map(repository => (
+                        <a key={repository.full_name} href="teste">
+                            <img
+                                src={repository.owner.avatar_url}
+                                alt={repository.owner.login}
+                            />
+                            <div>
+                                <strong>{repository.full_name}</strong>
+                                <p>{repository.description}</p>
+                            </div>
 
-                    <FiChevronRight size={20}/>
-                </a>
-            </Repositories>
-
-            <Repositories>
-                <a href="teste">
-                    <img
-                        src={logoImg}
-                        alt="Daniel Nogueira"
-                    />
-                    <div>
-                        <strong>dnogueir/node-challenge</strong>
-                        <p>Node challenge description</p>
-                    </div>
-
-                    <FiChevronRight size={20}/>
-                </a>
+                            <FiChevronRight size={20}/>
+                        </a>
+                    ))
+                }
             </Repositories>
         </>
     );
